0febef939e742d208fe5ca9b2475494e481eafe4
Natzsmart <iniobongeffiong40@gmail.com>
2024-02-13 23:33:49 +0100

    refractor the code using list comprehension

--- new_game.py
+++ new_game.py
@@ -16,10 +16,11 @@ while len(guessed_state)<50:
     # print(answer_states)
 
     if answer_states == 'Exit':
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_state:
-                missing_states.append(state)
+            #Using Python list comprehension break this lines of code into a single list.
+        missing_states = [state for state in all_states if state not in guessed_state]
+        # for state in all_states:
+        #     if state not in guessed_state:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv('magic.csv')
         break
@@ -34,45 +35,3 @@ while len(guessed_state)<50:
         t.write(answer_states)
 
 #urtle.exitonclick()
-
-
-# import turtle
-# import pandas
-
-# screen = turtle.Screen()
-# screen.title('New games')
-# image = 'blank_states_img.gif'
-# screen.addshape(image)
-# turtle.shape(image)
-
-# data = pandas.read_csv('50_states.csv')
-# all_states = data.state.to_list()
-# guessed_states = []
-
-
-# while len(guessed_states)<50:
-#     answer_states = screen.textinput(title=f'{len(guessed_states)}/50 Correct States', prompt='What is the next question?').title()
-#     print(answer_states)
-
-#     if answer_states == "Exit":
-#         missing_state = []
-#         for state in all_states:
-#             if state is not guessed_states:
-#                 missing_state.append(state)
-#         new_data = pandas.DataFrame(missing_state)
-#         new_data.to_csv('missing_state.csv')
-        
-#         # print(missing_state)
-#         break
-#     if answer_states in all_states:
-#         guessed_states.append(answer_states)
-#         t= turtle.Turtle()
-#         t.hideturtle()
-#         t.penup()
-#         state_data = data[data.state == answer_states]
-#         t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
-#         t.write(answer_states)
-
-# #screen.exitonclick()
-
-
